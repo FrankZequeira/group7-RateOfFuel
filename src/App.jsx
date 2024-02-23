45dc03273233f9c407d40621fbd82bf96c652541
Frank Zequeira <frankcarloszequeira@gmail.com>
2024-02-23 16:56:37 -0600

    Created nav bar and was able to use Bootstrap5 Just need some styling help

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FuelQuoteForm from './FuelQuoteForm';
+import ProfileManagement from './ProfileManagement';
+
+function App() {
+  return (
+    <Router>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Profile Management</Nav.Link>
+              <Nav.Link href="/fuel-quote">Fuel Quote</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="/" element={<ProfileManagement />} /> 
+        <Route path="/fuel-quote" element={<FuelQuoteForm />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
