fdd2676a277a3901d343e428d75bb858746b3c39
ksskatka <kasparslothskatka@gmail.com>
2024-02-23 20:16:13 -0600

    Create dummy quote history page and add route for it in App.jsx.
    Rename App.js to App.jsx.

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import FuelQuoteHistory from './FuelQuoteHistory';
+
+function App() {
+  return (
+    <div className="App">
+      {/* This is where we add the routes to our pages.
+          First import the path of the file (as done above for FuelQuoteHistory.jsx
+          Then add a Route with the same format as below (within the <Routes> tags)
+          The default page should have a path of '/' which makes it render when the site first opens */}
+      <Router>
+        <Routes>
+          <Route path='/fuelquotehistory' element={<FuelQuoteHistory/>}></Route>
+          {/* Add your routes here */}
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
